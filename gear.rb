a90e0feb093df4210f8af5a50b2b3bb6ee99624b
D.Mizuno <43818483+vviilloovv@users.noreply.github.com>
2019-07-02 11:05:07 +0900

    Add files via upload

--- gear.rb
+++ gear.rb
@@ -1,10 +1,11 @@
+require_relative 'wheel'
+
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-  def initialize(chainring, cog, rim, tire)
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, wheel = nil)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = wheel
   end
 
   def ratio
@@ -12,18 +13,8 @@ class Gear
   end
 
   def gear_inches
-    ratio * diameter
+    ratio * wheel.diameter
   end
 
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      # タイヤはリムの周りを囲むため2倍する
-      rim + (tire * 2)
-    end
-  end
 end
-
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-
-puts Gear.new(52, 11, 24, 1.25).gear_inches
 
---
+++ wheel.rb
@@ -0,0 +1,17 @@
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    # タイヤはリムの周りを囲むため2倍する
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end

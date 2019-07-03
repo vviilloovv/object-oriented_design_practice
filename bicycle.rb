7ac701d965d338eb917ea6aac96d6a7a99286d6b
D.Mizuno <43818483+vviilloovv@users.noreply.github.com>
2019-07-03 17:53:50 +0900

    Add files via upload

--- bicycle.rb
+++ bicycle.rb
@@ -1,8 +1,14 @@
 class Bicycle
-  attr_reader :size
+  attr_reader :size, :chain, :tire_size
 
   def initialize(args = {})
     @size = args[:size]
+    @chain = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+  end
+
+  def default_chain
+    "10-speed"
   end
 end
 
@@ -19,6 +25,10 @@ class RoadBike < Bicycle
       tire_size: "23",
       tape_color: tape_color }
   end
+
+  def default_tire_size
+    "23"
+  end
 end
 
 class MountainBike < Bicycle
@@ -33,12 +43,24 @@ class MountainBike < Bicycle
   def spares
     super.merge(rear_shock: rear_shock)
   end
+
+  def default_tire_size
+    "2.1"
+  end
 end
+
+road_bike = RoadBike.new(
+  size: "M",
+  tape_color: "red" )
+
+puts road_bike.tire_size
+puts road_bike.chain
 
 mountain_bike = MountainBike.new(
   size: "S",
   front_shock: "Manitou",
   rear_shock: "Fox" )
 
-puts mountain_bike.spares
+puts mountain_bike.tire_size
+puts mountain_bike.chain
 

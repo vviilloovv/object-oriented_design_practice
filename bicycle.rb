f1d913be543e5e66de1c9c9158a60d1b73eb94a9
D.Mizuno <43818483+vviilloovv@users.noreply.github.com>
2019-07-03 17:35:16 +0900

    Add files via upload

--- bicycle.rb
+++ bicycle.rb
@@ -1,33 +1,44 @@
 class Bicycle
-  attr_reader :style, :size, :tape_color, :front_shock, :rear_shock
+  attr_reader :size
 
-  def initialize(args)
-    @style = args[:style]
+  def initialize(args = {})
     @size = args[:size]
-    @tape_color = args[:tape_color]
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
-  end
-
-  # ちょっと危険な兆候
-  def spares
-    if style == :road
-      { chain: "10-speed",
-        tire_size: "23", # milimeters
-        tape_color: tape_color }
-    else
-      { chain: "10-speed",
-        tire_size: "2.1", #inches
-        rear_shock: rear_shock }
-    end
   end
 end
 
-bike = Bicycle.new(
-  style: :mountain,
+class RoadBike < Bicycle
+  attr_reader :tape_color
+
+  def initialize(args)
+    @type_color = args[:type_color]
+    super(args)
+  end
+
+  def spares
+    { chain: "10-speed",
+      tire_size: "23",
+      tape_color: tape_color }
+  end
+end
+
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
+mountain_bike = MountainBike.new(
   size: "S",
   front_shock: "Manitou",
-  rear_shock: "Fox")
+  rear_shock: "Fox" )
 
-puts bike.spares
+puts mountain_bike.spares
 

ff1e2d62863b08504604826397fd97a12c29ac7c
D.Mizuno <43818483+vviilloovv@users.noreply.github.com>
2019-07-05 15:46:09 +0900

    Add files via upload

--- bicycle.rb
+++ bicycle.rb
@@ -1,5 +1,3 @@
-require 'forwardable'
-
 class Bicycle
   attr_reader :size, :parts
 
@@ -13,6 +11,7 @@ class Bicycle
   end
 end
 
+require 'forwardable'
 class Parts
   extend Forwardable
   def_delegators :@parts, :size, :each
@@ -27,37 +26,20 @@ class Parts
   end
 end
 
-class Part
-  attr_reader :name, :description, :needs_spare
-
-  def initialize(args)
-    @name = args[:name]
-    @description = args[:description]
-    @needs_spare = args.fetch(:needs_spare, true)
-  end
-end
-
+require 'ostruct'
 module PartsFactory
-  def self.build(config,
-                 part_class = Part,
-                 parts_class = Parts)
-
+  def self.build(config, parts_class = Parts)
     parts_class.new(
-      config.collect do |part_config|
-        part_config.new(
-          name: part_config[0],
-          description: part_config[1],
-          needs_spare: part_config.fetch(2, true))
-      end)
+      config.map { |part_config| create_part(part_config) })
+  end
+
+  def self.create_part(part_config)
+    OpenStruct.new(
+      name: part_config[0],
+      description: part_config[1],
+      needs_spare: part_config.fetch(2, true))
   end
 end
-
-chain = Part.new(name: "chain", description: "10-speed")
-road_tire = Part.new(name: "tire_size", description: "23")
-tape = Part.new(name: "tape_color", description: "red")
-mountain_tire = Part.new(name: "tire_size", description: "2.1")
-rear_shock = Part.new(neme: "rear_shock", description: "Fox")
-front_shock = Part.new(name: "front_shock", description: "Manitou", needs_spare: false)
 
 road_config = 
   [["chain", "10-speed"],
@@ -70,25 +52,15 @@ mountain_config =
    ["front_shock", "Manitou", false],
    ["rear_shock", "Fox"]]
 
-road_parts = PartsFactory.build(road_config)
-mountain_parts = PartsFactory.build(mountain_config)
-
 road_bike = Bicycle.new(
   size: "L",
-  parts: Parts.new([chain,
-                    road_tire,
-                    tape]))
+  parts: PartsFactory.build(road_config))
 
-puts road_bike.size
-puts road_bike.spares
+p road_bike.spares
 
 mountain_bike = Bicycle.new(
   size: "S",
-  parts: Parts.new([chain,
-                    mountain_tire,
-                    front_shock,
-                    rear_shock]))
+  parts: PartsFactory.build(mountain_config))
 
-puts mountain_bike.size
-puts mountain_bike.spares
+p mountain_bike.spares
 
